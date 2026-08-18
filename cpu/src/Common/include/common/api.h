9742f542b56742593eadf4c12f7b68d1732849cb
LAPTOP-K4QIF604\kenna <kennakaxx@gmail.com>
2026-08-18 12:54:24 +0900

    マクロによるDLLインポート・エクスポートの切り替えの仕組みを分離

---
+++ cpu/src/Common/include/common/api.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#if defined(BUILD_Common)
+#define Common_API __declspec(dllexport)
+#elif defined(USE_Common)
+#define Common_API __declspec(dllimport)
+#else
+#define Common_API
+#endif
--- cpu/src/Common/include/common/test.h
+++ cpu/src/Common/include/common/test.h
@@ -1,11 +1,5 @@
 ﻿#pragma once
 
-#if defined(BUILD_Common)
-#define Common_API __declspec(dllexport)
-#elif defined(USE_Common)
-#define Common_API __declspec(dllimport)
-#else
-#define Common_API
-#endif
+#include "api.h"
 
 Common_API int common_test();
