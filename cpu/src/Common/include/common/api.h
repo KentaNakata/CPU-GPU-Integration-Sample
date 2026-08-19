915fc306f35ee4c64200e54c30f759d440357c90
KentaNakata <123381368+KentaNakata@users.noreply.github.com>
2026-08-19 10:36:34 +0900

    Refine comments on macro definitions in api.h
    Updated comments for clarity regarding macro definitions.

--- cpu/src/Common/include/common/api.h
+++ cpu/src/Common/include/common/api.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-// ビルド時はBUILD_Commonマクロを定義。これによりビルド時は、シンボルがエクスポート設定になる
-// インストール時はUSE_Commonマクロを定義。これにより外部からdllが参照された時は、シンボルがインポート設定になる
+// ビルド時はBUILD_Commonマクロが定義される。これによりビルド時は、シンボルがエクスポート設定になる
+// インストール時はUSE_Commonマクロが定義される。これにより外部からdllが参照された時は、シンボルがインポート設定になる
 
 #if defined(BUILD_Common)
 #define Common_API __declspec(dllexport)
